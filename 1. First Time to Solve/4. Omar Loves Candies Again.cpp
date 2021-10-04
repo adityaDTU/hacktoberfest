e5618b9834c21a45d51d4e9d8ffc6d9199fcd78e
Aditya Singh <samant04aditya@gmail.com>
2021-10-04 12:57:17 +0530

    first A2OJ ladder

---
+++ 1. First Time to Solve/4. Omar Loves Candies Again.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#include<stdlib.h>
+using namespace std;
+
+// Author - ADITYA SINGH (https://github.com/adityanjr)
+
+#define ll              long long
+#define pb              push_back
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n)       int *arr=new int[n];
+#define t(x)            int t; cin>>t; while(t--)
+#define fa(n)           for(int i=0; i<n; i++)
+#define f(x,y)          for(int i=x; i<=y; i++)
+
+
+void a_d_i() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main() {
+
+	a_d_i();
+
+	t(x) {
+		string s;
+		int arr[256] = {0};
+		cin >> s;
+		fa(s.length()) {
+			arr[s[i]]++;
+		}
+		int max = 0;
+		char index;
+		fa(256) {
+			if (arr[i] > max) {
+				max = arr[i];
+				index = i;
+			}
+		}
+		cout << max << " " << index << endl;
+	}
+
+	return 0;
+
+}
